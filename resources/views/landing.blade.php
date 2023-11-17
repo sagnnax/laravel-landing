f23e9605dfafb661fc436f4afb36bbc6e39684a6
sagnnax <axel.gustavo2001@outlook.com>
2023-11-17 15:55:23 -0600

    initial code

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,194 @@
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="/resources/img/favicon.png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+    <title>Cars</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <img src="./src/logo.png" width="75" height="100" float="right" />
+    <h1 class="mt-2" align="center">Porsche<br><small style="color: rgb(173, 153, 62);">La perfección nunca es el principio</small>
+    </h1>
+    <div class="row justify-content-end">
+        <div class="col-auto">
+            <a href="#1" class="btn btn-dark">La variante más agresiva</a>
+            <a href="#2" class="btn btn-primary">Modificaciones</a>
+            <a href="#3" class="btn btn-success">Colores populares</a>
+            <a href="#4" class="btn btn-warning">Escenario de leyendas</a>
+        </div>
+    </div>
+</head>
+<title>
+    Cars
+</title>
+<body class="container" style="background-color: #387d80;">
+    <a id="1"></a>
+    <h2 class="mt-2">GT3 RS 991</h2>
+    <img src="/resources/img/Porsche.jpg" width="390" height="190" />
+    <p>
+        El Porsche 911 GT3 RS 991 es una variante de alto rendimiento del Porsche 911 de la generación 991, fabricado
+        entre 2013 y 2019. Este automóvil cuenta con un motor de 4.0 litros naturalmente aspirado, tracción trasera y
+        una gran cantidad de mejoras aerodinámicas. Su construcción ligera y su interior enfocado en las carreras lo
+        hacen ideal para su uso en circuitos, ofreciendo una experiencia de conducción emocionante y precisa.</br>
+    </p>
+    <a id="2"></a>
+    <h2 class="mt-2">RWB</h2>
+    <video width="640" height="360" controls>
+        <source src="./src/rwb.mp4" type="video/mp4">
+    </video>
+    <p>
+        Por sus siglas RAUH-Welt Begriff (RWB) es un preparador de prestigio dentro de la comunidad Porsche.Su
+        propietario, Akira Nakai,
+        muestra una preferencia desmesurada por los pasos de rueda ensanchados, las llantas de profunda garganta y las
+        suspensiones rebajadas
+        hasta casi rozar el suelo. Las modificaciones mecánicas permanecen en un discreto segundo plano, eclipsadas por
+        una apariencia espectacular.
+    </p>
+    <br>
+    <a id="3"></a>
+    <div class="row">
+        <h2 class="mt-2">Top 3 colores más populares de Porsche</h2>
+        <div class="col">
+            <img src="./src/guardsRed.jpg" width="270" height="190" />
+            <p>Guards Red</p>
+        </div>
+        <div class="col">
+            <img src="./src/mexicoBlue.jpg" width="265" height="190" />
+            <p>Mexico Blue</p>
+        </div>
+        <div class="col">
+            <img src="./src/gtSilver.jpg" width="295" height="190" />
+            <p>GT Silver</p>
+        </div>
+    </div>
+    <a id="4"></a>
+    <h2 class="mt-2">Porsche en Nürburgring</h2>
+    <p>
+        El "Circuito de Nürburgring" es un famoso circuito de carreras situado en Nürburg, Renania-Palatinado, Alemania.
+        Hay dos pistas principales en Nürburgring: el Nürburgring Nordschleife y el Nürburgring GP-Strecke (circuito
+        Grand Prix). A menudo, cuando se menciona el "Nürburgring", se hace referencia al Nordschleife, que es una de
+        las pistas de carreras más icónicas y desafiantes del mundo.
+    </p>
+    <p>
+        El Nürburgring es conocido por ser un desafío extremo para los conductores debido a su longitud y complejidad,
+        con curvas cerradas, cambios de elevación y un terreno accidentado. También es un lugar icónico para que los
+        fabricantes de automóviles prueben y ajusten sus vehículos de alto rendimiento, ya que ofrece una variedad de
+        condiciones de manejo en una sola vuelta.
+    </p>
+    <br>
+    <div class="container">
+        <div class="row justify-content-start">
+            <div class="col-4">
+                <img src="https://files.porsche.com/filestore/image/multimedia/none/christophorus-issue381-article10-margin-01/normal/5b4b4ec3-19e4-11e7-9f74-0019999cd470;s55/porsche-normal.jpg"
+                    width="400" height="225">
+                <br>
+                <b>Fecha: 17 de julio de 1927
+                Victoria: Otto Merz
+                Vehículo: Mercedes Benz modelo S
+                Distancia: 18 vueltas de 28,265 km (Nordschleife y Südschleife)
+                Velocidad media del vencedor: 101,8 km/h</b>
+            </div>
+            <div class="col-4">
+                <img src="https://files.porsche.com/filestore/image/multimedia/none/christophorus-issue381-article10-margin-02/normal/62423791-19e4-11e7-9f74-0019999cd470;s55/porsche-normal.jpg"
+                    width="400" height="225">
+                <br>
+                <b>Fecha: 26 de julio de 1936
+                Victoria: Bernd Rosemeyer
+                Vehículo: Auto Union modelo C
+                Distancia: 22 vueltas de 22,810 km (Nordschleife)
+                Velocidad media del vencedor: 131,65 km/h</b>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <img src="https://files.porsche.com/filestore/image/multimedia/none/christophorus-issue381-article10-margin-03/normal/76df8899-19e4-11e7-9f74-0019999cd470;s55/porsche-normal.jpg"
+                    width="400" height="225">
+                <br>
+                <b>Fecha: 27 de mayo de 1956
+                Victoria (clase S 1,5 litros): Wolfgang Graf Berghe von Trips, Umberto Maglioli
+                Vehículo: Porsche 550 A Spyder
+                Distancia: 44 vueltas de 22,810 km (Nordschleife)
+                Velocidad media de los vencedores en la general: 129,8 km/h</b>
+            </div>
+            <div class="col-4">
+                <img src="https://files.porsche.com/filestore/image/multimedia/none/christophorus-issue381-article10-margin-04/normal/7e88f8d2-19e4-11e7-9f74-0019999cd470;s55/porsche-normal.jpg"
+                    width="400" height="225">
+                <br>
+                <b>Fecha: 28 de mayo de 1967
+                Victoria: Udo Schütz, Joe Buzzetta
+                Vehículo: Porsche 910
+                Distancia: 44 vueltas de 22,810 km (Nordschleife)
+                Velocidad media de los vencedores: 145,5 km/h</b>
+            </div>
+        </div>
+        <div class="row justify-content-end">
+            <div class="col-4">
+                <img src="https://files.porsche.com/filestore/image/multimedia/none/christophorus-issue381-article10-margin-05/normal/8e7b55a4-19e4-11e7-9f74-0019999cd470;s55/porsche-normal.jpg"
+                    width="400" height="225">
+                <br>
+                <b>Fecha: 31 de mayo de 1970
+                Victoria: Vic Elford, Kurt Ahrens
+                Vehículo: Porsche 908/03 Spyder
+                Distancia: 44 vueltas de 22,810 km (Nordschleife)
+                Velocidad media de los vencedores: 165,0 km/h</b>
+            </div>
+            <div class="col-4">
+                <img src="https://files.porsche.com/filestore/image/multimedia/none/christophorus-issue381-article10-margin-06/normal/c8fcc4c4-19f0-11e7-9f74-0019999cd470;s55/porsche-normal.jpg"
+                    width="400" height="225">
+                <br>
+                <b>Fecha: 28 de mayo de 1983 (entrenamiento), 29 de mayo de 1983 (carrera)
+                Victoria: Jochen Mass, Jacky Ickx
+                Vehículo: Porsche 956 C
+                Vuelta récord: Stefan Bellof (6:11,13 min.)
+                Distancia: 44 vueltas de 20,832 km (Nordschleife)
+                Velocidad media de los vencedores: 165,0 km/h</b>
+            </div>
+        </div>
+        <div class="row justify-content-around">
+            <div class="col-4">
+                <img src="https://files.porsche.com/filestore/image/multimedia/none/christophorus-issue381-article10-margin-07/normal/e3627ee4-1ab6-11e7-9f74-0019999cd470;s55/porsche-normal.jpg"
+                    width="400" height="225">
+                <br>
+                <b>Fecha: 12/13 de junio de 1993
+                Victoria: Franz Konrad, Frank Katthöfer, Örnulf Wirdheim, Antonio de Azevedo
+                Vehículo: Porsche 911 Carrera RSR 3.8</b>
+            </div>
+            <div class="col-4">
+                <img src="https://files.porsche.com/filestore/image/multimedia/none/christophorus-issue381-article10-margin-08/normal/f026f4b8-19f0-11e7-9f74-0019999cd470;s55/porsche-normal.jpg"
+                    width="400" height="225">
+                <br>
+                <b>Fecha: 15/16 de mayo de 2010
+                Victoria: Jörg Bergmeister, Marco Holzer, Richard Lietz, Martin Ragginger
+                Vehículo: 911 GT3 R Hybrid</b>
+            </div>
+        </div>
+        <div class="row justify-content-between">
+            <div class="col-4">
+                <img src="https://files.porsche.com/filestore/image/multimedia/none/christophorus-issue381-article10-margin-09/normal/6b53ef17-19f1-11e7-9f74-0019999cd470;s55/porsche-normal.jpg"
+                    width="400" height="225">
+                <br>
+                <b>Fecha: 25 de junio de 2011
+                Victoria: René Rast
+                Distancia: 6 vueltas de 22,810 km (Nordschleife)
+                Participantes: 100 de 25 países
+                Vehículo: Porsche 911 GT3 Cup</b>
+            </div>
+            <div class="col-4">
+                <img src="https://files.porsche.com/filestore/image/multimedia/none/christophorus-issue381-article10-margin-10/normal/73c09062-19f1-11e7-9f74-0019999cd470;s55/porsche-normal.jpg"
+                    width="400" height="225">
+                <br>
+                <b>Fecha: 4 de septiembre de 2013
+                Récord: Marc Lieb
+                Vehículo: Porsche 918 Spyder
+                Velocidad media: 179,5 km/h</b>
+            </div>
+        </div>
+    </div>
+</body>
+<br>
+<footer style="text-align: center;">
+    <p>Porsche© 2023 - Axel Gustavo</p>
+    <p>zS20006742@estudiantes.uv.mx</p>
+</footer>
+</html>
